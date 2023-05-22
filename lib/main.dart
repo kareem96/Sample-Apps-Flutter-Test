fa1675f82abc5c3cad61613e9724ddbec72ef73c
kareem96 <ak339444@gmail.com>
2023-05-22 11:14:52 +0700

    Flutter apps test

--- lib/main.dart
+++ lib/main.dart
@@ -28,70 +28,90 @@ class HomePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: Text('Contoh Aplikasi Flutter')),
+      appBar: AppBar(title: const Text('Contoh Aplikasi Flutter')),
       body: Padding(
         padding: const EdgeInsets.all(16.0),
         child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
           children: [
             TextField(
               controller: _textEditingController,
               keyboardType: TextInputType.number,
-              decoration: InputDecoration(labelText: 'Masukkan N'),
+              decoration: const InputDecoration(labelText: 'Masukkan N'),
             ),
-            SizedBox(height: 16.0),
-            ElevatedButton(
-              onPressed: () {
-                int? n = int.tryParse(_textEditingController.text);
-                if (n != null) {
-                  BlocProvider.of<NumberSequenceBloc>(context)
-                      .add(GenerateSequenceEvent(n));
-                }
-              },
-              child: Text('Deret Angka 1->N'),
+            const SizedBox(height: 16.0),
+            Row(
+              children: [
+                const Spacer(flex: 20,),
+                ElevatedButton(
+                  onPressed: () {
+                    int? n = int.tryParse(_textEditingController.text);
+                    if (n != null) {
+                      BlocProvider.of<NumberSequenceBloc>(context)
+                          .add(GenerateSequenceEvent(n));
+                    }
+                  },
+                  child: const Text('1'),
+                ),
+                const Spacer(),
+                ElevatedButton(
+                  onPressed: () {
+                    int? n = int.tryParse(_textEditingController.text);
+                    if (n != null) {
+                      BlocProvider.of<NumberSequenceBloc>(context)
+                          .add(GenerateReverseSequenceEvent(n));
+                    }
+                  },
+                  child: Text('2'),
+                ),
+                const Spacer(flex: 20,),
+              ],
             ),
-            ElevatedButton(
-              onPressed: () {
-                int? n = int.tryParse(_textEditingController.text);
-                if (n != null) {
-                  BlocProvider.of<NumberSequenceBloc>(context)
-                      .add(GenerateReverseSequenceEvent(n));
-                }
-              },
-              child: Text('Deret Angka 1->N->1'),
+            Row(
+              children: [
+                const Spacer(flex: 20,),
+                ElevatedButton(
+                  onPressed: () {
+                    int? n = int.tryParse(_textEditingController.text);
+                    int increment = 11; // Ubah sesuai kebutuhan
+                    if (n != null) {
+                      BlocProvider.of<NumberSequenceBloc>(context)
+                          .add(GenerateIncrementalSequenceEvent(n, increment));
+                    }
+                  },
+                  child: Text('3'),
+                ),
+                const Spacer(),
+                ElevatedButton(
+                  onPressed: () {
+                    int? n = int.tryParse(_textEditingController.text);
+                    int multiple1 = 5; // Ubah sesuai kebutuhan
+                    int multiple2 = 7; // Ubah sesuai kebutuhan
+                    if (n != null) {
+                      BlocProvider.of<NumberSequenceBloc>(context)
+                          .add(GenerateReplacementSequenceEvent(
+                          n, multiple1, multiple2));
+                    }
+                  },
+                  child: Text('4'),
+                ),
+                const Spacer(flex: 20,),
+              ],
             ),
-            ElevatedButton(
-              onPressed: () {
-                int? n = int.tryParse(_textEditingController.text);
-                int increment = 11; // Ubah sesuai kebutuhan
-                if (n != null) {
-                  BlocProvider.of<NumberSequenceBloc>(context)
-                      .add(GenerateIncrementalSequenceEvent(n, increment));
-                }
-              },
-              child: Text('Deret Angka dengan Penambahan'),
-            ),
-            ElevatedButton(
-              onPressed: () {
-                int? n = int.tryParse(_textEditingController.text);
-                int multiple1 = 5; // Ubah sesuai kebutuhan
-                int multiple2 = 7; // Ubah sesuai kebutuhan
-                if (n != null) {
-                  BlocProvider.of<NumberSequenceBloc>(context)
-                      .add(GenerateReplacementSequenceEvent(
-                      n, multiple1, multiple2));
-                }
-              },
-              child: Text('Deret Angka dengan Penggantian'),
-            ),
-            BlocBuilder<NumberSequenceBloc, NumberSequenceState>(
-              builder: (context, state) {
-                if (state is NumberSequenceGenerated) {
-                  return Text(state.sequence);
-                }
-                return SizedBox.shrink();
-              },
-            ),
+            const SizedBox(height: 40,),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.start,
+              children: [
+                const Text("Result : "),
+                BlocBuilder<NumberSequenceBloc, NumberSequenceState>(
+                  builder: (context, state) {
+                    if (state is NumberSequenceGenerated) {
+                      return Text(state.sequence);
+                    }
+                    return const SizedBox.shrink();
+                  },
+                ),
+              ],
+            )
           ],
         ),
       ),
